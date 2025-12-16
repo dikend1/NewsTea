e32aef78fc5ec5dae80ee06287cac089f3516a86
qairatulyyerasyl16 <kairatulyerasil@gmail.com>
2025-12-16 20:54:09 +0500

    small refinements

--- js/write.js
+++ js/write.js
@@ -7,6 +7,7 @@ function publish(){
     const title = document.getElementById('write-title-input').value;
     const text = document.getElementById('write-text-input').value;
     const date = year + "-" + month + "-" + day;
+    const author = "myself";
     if( title === '' ){
         alert("Please enter a title");
         return;
@@ -19,7 +20,7 @@ function publish(){
     const id = title.replaceAll(' ', '_') + '_' + crypto.randomUUID();
 
     const stories = JSON.parse(localStorage.getItem('stories') || '[]');
-    stories.push({ id, title, main: text , date: date });
+    stories.push({ id, title, main: text , date: date , author: author});
     localStorage.setItem('stories', JSON.stringify(stories));
 
     open('main.html');
