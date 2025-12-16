9733eb7205d98d0469ebd26340e29bdac9f3eb10
qairatulyyerasyl16 <kairatulyerasil@gmail.com>
2025-12-17 00:56:55 +0500

    added Card, Write, Stories

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ let stories = JSON.parse(localStorage.getItem("stories"));
 
 
 for (let i = 0; i < stories.length; i++) {
+    let href = document.createElement("a");
+    href.href = "../html/Card.html?id=" + stories[i].id;
+    href.style = "text-decoration: none; color: black;";
     let story = document.createElement("article");
-    story.id = stories[i].id;
     story.className = "news-card";
+    let profile = document.createElement("div");
+    profile.className = "news-profile";
+    let profileImg = document.createElement("img");
+    profileImg.src = "../img/myself.JPG";
+    let profileText = document.createElement("p");
+    profileText.innerText = stories[i].author;
+    profile.appendChild(profileImg);
+    profile.appendChild(profileText);
     let title = document.createElement("h2");
     title.innerText = stories[i].title;
     title.className = "news-title";
@@ -16,23 +26,29 @@ for (let i = 0; i < stories.length; i++) {
     let date = document.createElement("div");
     date.className = "news-meta";
     date.innerText = stories[i].date;
+    story.appendChild(profile);
     story.appendChild(title);
     story.appendChild(text);
     story.appendChild(date);
-    parent.appendChild(story);
+    href.appendChild(story);
+    parent.appendChild(href);
 }
 
 
-// <article className="news-card">
-//     <h2 className="news-title">
+// <article class="news-card">
+//     <div class="news-profile">
+//         <img src="../img/pfp.png" alt="not found"/>
+//         <p class="news-author">Maksatov Dias</p>
+//     </div>
+//     <h2 class="news-title">
 //         Kazakhstan Launches New Satellite Made Entirely of Recycled Laptops from 2008.
 //     </h2>
-//     <p className="news-text">
+//     <p class="news-text">
 //         October 23rd 2025 Kazakhstan's first satellite has been launched.
 //         The satellite was made entirely from laptops of recycled materials.
 //         The satellite is named...
 //     </p>
-//     <div className="news-meta">
+//     <div class="news-meta">
 //         65 min read
 //     </div>
 // </article>
