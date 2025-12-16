3ec1d8dd206b164e1d157cbd344649ad9a83bef9
qairatulyyerasyl16 <kairatulyerasil@gmail.com>
2025-12-17 02:28:22 +0500

    added story deleting and deleted comments

--- js/stories.js
+++ js/stories.js
@@ -9,26 +9,63 @@ for (let i = 0; i < stories.length; i++) {
     if(stories[i].author !== "myself"){
         continue;
     }
+
+    let row = document.createElement("div");
+    row.className = "story-row";
+    row.id = stories[i].id;
+
+
     let href = document.createElement("a");
     href.href = "../html/Write.html?id=" + stories[i].id;
     href.style = "text-decoration: none; color: black;";
+
     let article = document.createElement("article");
     article.className = "stories-card";
+
     let info = document.createElement("div");
     info.className = "stories-info";
+
     let title = document.createElement("h2");
     title.className = "stories-title"
     title.innerText = stories[i].title;
+
     let date = document.createElement("div");
     date.className = "stories-meta"
     date.innerText = stories[i].date + " - " + stories[i].main.length;
+
+    let btn = document.createElement("button");
+    btn.className = "stories-btn";
+    btn.innerText = "x";
+    btn.onclick = () => deleteStory(stories[i].id);
+
+
     info.appendChild(title);
     info.appendChild(date);
+
     article.appendChild(info);
+
     href.appendChild(article);
-    parent.appendChild(href);
+
+    row.appendChild(href);
+    row.appendChild(btn);
+
+    parent.appendChild(row);
+
     counter++;
     document.getElementById("counter").innerText = counter;
+}
+
+function deleteStory(id){
+
+    stories = stories.filter((story) => story.id === id);
+
+    localStorage.setItem("stories", JSON.stringify(stories));
+
+    document.getElementById(id).remove();
+
+    counter--;
+    document.getElementById("counter").innerText = counter;
+
 }
 
 // <article class="stories-card">
